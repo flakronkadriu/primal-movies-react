c78b3aa8156d0c4683555c77b37ae94bf9d7ed65
Roni <flakron.kadriu@gmail.com>
2019-11-13 13:30:43 +0100

    keys removed

--- src/environments.js
+++ src/environments.js
@@ -1,7 +1,7 @@
 var environment = {
     movieDbBaseUrl: "https://api.themoviedb.org/3",
-    YOUTUBE_API_KEY: "AIzaSyCh_gyw-T4oBwpVfKqfpbfp_9Pezr2fqZg",
-    MOVIE_DB_API_KEY: "9c4e3cafe732f377215e4a4f7e853a38"
+    YOUTUBE_API_KEY: "",
+    MOVIE_DB_API_KEY: ""
 };
 
 export { environment };
